4fbbab624e462e18cb05b73ca448276ed94c5960
seanchok <seanchok@gmail.com>
2021-01-19 15:14:19 +0800

    mobile: rename app to scraper

--- mobile-app/ios/scraperTests/scraperTests.m
+++ mobile-app/ios/scraperTests/scraperTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface wherewewowTests : XCTestCase
+@interface scraperTests : XCTestCase
 
 @end
 
-@implementation wherewewowTests
+@implementation scraperTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
