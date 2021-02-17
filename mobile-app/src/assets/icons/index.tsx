28ccefdb0349054db48a5409f3029aa9d24c6344
seanchok <seanchok@gmail.com>
2021-02-17 13:23:46 +0800

    mobile: minor refactor

--- mobile-app/src/assets/icons/index.tsx
+++ mobile-app/src/assets/icons/index.tsx
@@ -1,4 +1,1 @@
-// export { ReactComponent as ExampleIcon } from './example-icon.[png,jpg,...]';
-
-// todo: remove next line
 export {};
--- mobile-app/src/assets/images/index.tsx
+++ mobile-app/src/assets/images/index.tsx
@@ -1,4 +1,1 @@
-// export { ReactComponent as ExampleImage } from './example-image.[svg,png,jpg,...]';
-
-// todo: remove next line
 export {};
