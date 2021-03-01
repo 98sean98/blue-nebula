4a4161e22f46beaf6a0a7ce83923ec82edbb4e30
seanchok <seanchok@gmail.com>
2021-03-01 16:25:54 +0800

    mobile: enable read / write async storage for maker config data

--- mobile-app/src/components/layer/AsyncStorageLayer.tsx
+++ mobile-app/src/components/layer/AsyncStorageLayer.tsx
@@ -7,20 +7,17 @@ import { capitalCase } from 'change-case';
 import { RootState } from '@reduxApp';
 import { setControlEntities } from '@reduxApp/control/actions';
 import { SetControlEntities } from '@reduxApp/control/types';
-import {
-  // setMakerConfig,
-  setSetups,
-} from '@reduxApp/builder/actions';
-import {
-  // SetMakerConfig,
-  SetSetups,
-} from '@reduxApp/builder/types';
+import { setMakerConfig, setSetups } from '@reduxApp/builder/actions';
+import { SetMakerConfig, SetSetups } from '@reduxApp/builder/types';
 
 import {
   ConvertibleState,
   convertStateWithTimestamps,
 } from '@utilities/functions/convertStateWithTimestamps';
-// import { ConvertibleObject, convertObjectWithTimestampKeys } from '@utilities/functions/convertObjectWithTimestampKeys';
+import {
+  ConvertibleObject,
+  convertObjectWithTimestampKeys,
+} from '@utilities/functions/convertObjectWithTimestampKeys';
 
 const renderAlert = (type: 'reading' | 'writing') =>
   Alert.alert(
@@ -32,10 +29,9 @@ export const AsyncStorageLayer: FC = ({ children }) => {
   const dispatch = useDispatch();
 
   const { controlEntities } = useSelector((state: RootState) => state.control);
-  const {
-    setups,
-    // makerConfig
-  } = useSelector((state: RootState) => state.builder);
+  const { setups, makerConfig } = useSelector(
+    (state: RootState) => state.builder,
+  );
 
   const readStorage = useCallback(async (storageKey: string): Promise<
     unknown
@@ -64,17 +60,17 @@ export const AsyncStorageLayer: FC = ({ children }) => {
           dispatch(setSetups(stateWithConvertedTimestamps as SetSetups));
         }
       });
-      // readStorage('makerConfig').then((state) => {
-      //   if (typeof state !== 'undefined') {
-      //     const stateWithConvertedTimestamps = convertObjectWithTimestampKeys(
-      //       state as ConvertibleObject,
-      //       ['updatedAt'],
-      //     );
-      //     dispatch(
-      //       setMakerConfig(stateWithConvertedTimestamps as SetMakerConfig),
-      //     );
-      //   }
-      // });
+      readStorage('makerConfig').then((state) => {
+        if (typeof state !== 'undefined') {
+          const stateWithConvertedTimestamps = convertObjectWithTimestampKeys(
+            state as ConvertibleObject,
+            ['updatedAt'],
+          );
+          dispatch(
+            setMakerConfig(stateWithConvertedTimestamps as SetMakerConfig),
+          );
+        }
+      });
     } catch (error) {
       console.log(error);
       renderAlert('reading');
@@ -122,19 +118,19 @@ export const AsyncStorageLayer: FC = ({ children }) => {
   }, [writeStorage, setups]);
 
   // --- write setups data when it is updated ---
-  // useEffect(() => {
-  //   try {
-  //     // artificially delay updates to async storage by 500ms to avoid writing empty state on initial load
-  //     const timeout = setTimeout(() => {
-  //       writeStorage('makerConfig', makerConfig).then();
-  //       console.log('successfully wrote app makerConfig data into storage!');
-  //     }, 500);
-  //     return () => clearTimeout(timeout);
-  //   } catch (error) {
-  //     console.log(error);
-  //     renderAlert('writing');
-  //   }
-  // }, [writeStorage, makerConfig]);
+  useEffect(() => {
+    try {
+      // artificially delay updates to async storage by 500ms to avoid writing empty state on initial load
+      const timeout = setTimeout(() => {
+        writeStorage('makerConfig', makerConfig).then();
+        console.log('successfully wrote app makerConfig data into storage!');
+      }, 500);
+      return () => clearTimeout(timeout);
+    } catch (error) {
+      console.log(error);
+      renderAlert('writing');
+    }
+  }, [writeStorage, makerConfig]);
 
   return <>{children}</>;
 };
