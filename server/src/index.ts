76e78bd2a105b53272f725cc6ceb41e4a950c7b2
seanchok <seanchok@gmail.com>
2021-03-04 17:38:15 +0800

    server: install and start an express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,58 @@
-import { PrismaClient } from '@prisma/client';
+import * as env from 'dotenv';
+import express, { Application } from 'express';
+import cors, { CorsOptions } from 'cors';
+import * as bodyParser from 'body-parser';
+// import { server } from './server';
+import { database } from './middleware';
 
-import { sum } from '@utilities/sum';
+env.config();
 
-const prisma = new PrismaClient();
+const app: Application = express();
 
-/**
- *
- */
-async function main() {
-  const users = await prisma.user.findMany();
-  console.log({ users });
-  console.log('sum', sum(1, 3));
-}
+// cors
+const corsOptions: CorsOptions = {
+  origin: [
+    // todo: put server url
+    '',
+  ].concat(
+    process.env.NODE_ENV === 'development' ? ['http://localhost:3000'] : [],
+  ),
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
-main()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+// check if database is connected for each request
+app.use(database);
+
+// apollo server instance
+// const graphqlPath = '/graphql';
+const port = process.env.PORT || 4000;
+// server.applyMiddleware({
+//   app,
+//   cors: corsOptions,
+//   path: graphqlPath,
+//   bodyParserConfig: {
+//     limit: '20mb',
+//   },
+// });
+
+// root route
+app.get('/', (_req, res) => {
+  res.status(200).send('ok');
+});
+
+// body parser for auth REST api
+app.use(bodyParser.json({ type: 'application/json' }));
+
+// auth REST api
+// app.use('/auth', auth);
+
+// listener
+app.listen(port, () => {
+  console.log(
+    `Graphql server is running on ${port}${
+      // server.graphqlPath
+      ''
+    }`,
+  );
+});
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,16 @@
-export {};
+import { ApolloServer } from 'apollo-server-express';
+// import { applyMiddleware } from 'graphql-middleware';
+// import { schema } from './api';
+// import { permissions } from './shield/permissions';
+// import { createContext } from './context';
+
+export const server = new ApolloServer({
+  // schema: applyMiddleware(schema,
+  //     // permissions
+  // ),
+  // context: ({ req }) => {
+  //     return createContext(req);
+  // },
+  playground: true,
+  debug: process.env.NODE_ENV === 'development',
+});
