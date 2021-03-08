c170cbb14fec18d5030c1a0ef61396bb9ff05cc8
seanchok <seanchok@gmail.com>
2021-03-08 23:49:40 +0800

    serverless: fix serverless packaging by moving generated type-graphql into src

--- serverless/src/schema/graphqlSchema.ts
+++ serverless/src/schema/graphqlSchema.ts
@@ -1,5 +1,5 @@
 import { buildSchemaSync } from 'type-graphql';
-import { UserCrudResolver } from '@generated/type-graphql';
+import { UserCrudResolver } from '../artifacts/type-graphql';
 
 export const graphqlSchema = buildSchemaSync({
   resolvers: [UserCrudResolver],
