7a6a55b0e71adc7f8beb9608708805914250c836
seanchok <seanchok@gmail.com>
2021-03-15 23:47:36 +0800

    mobile: add bldc models as a control entity

---
+++ mobile-app/src/models/control-entity/Brake.ts
@@ -0,0 +1,4 @@
+export enum Brake {
+  Low = 0,
+  High = 1,
+}
--- mobile-app/src/models/control-entity/index.ts
+++ mobile-app/src/models/control-entity/index.ts
@@ -1,5 +1,6 @@
 export * from './Direction';
 export * from './Enable';
+export * from './Brake';
 
 export * from './ControlEntity';
 export * from './ControlEntityEnum';
@@ -7,3 +8,4 @@ export * from './ControlEntities';
 
 export * from './StepperMotor';
 export * from './DCMotor';
+export * from './BLDCMotor';
