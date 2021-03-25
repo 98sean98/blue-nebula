d7195c7566e971abb9d509b12ba4e3533f0249c6
seanchok <seanchok@gmail.com>
2021-03-25 18:23:18 +0800

    serverless: remove serverless/ directory, and github actions

--- serverless/__tests__/hello-world.ts
+++
@@ -1,7 +0,0 @@
-describe('basic test', () => {
-  test('should test hello world', () => {
-    expect('hello world').toEqual('hello world');
-  });
-});
-
-export {};
--- serverless/src/utilities/jsonMiddify.ts
+++
@@ -1,7 +0,0 @@
-import middy from '@middy/core';
-import middyJsonBodyParser from '@middy/http-json-body-parser';
-
-import { ValidatedEventAPIGatewayProxyEvent } from '@utilities/apiGateway';
-
-export const jsonMiddify = (handler: ValidatedEventAPIGatewayProxyEvent<any>) =>
-  middy(handler).use(middyJsonBodyParser());
