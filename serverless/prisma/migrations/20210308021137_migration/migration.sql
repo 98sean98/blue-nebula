c4ae64580e1323864ad3ae2b77f9bcad2657cf63
Sean Chok <seanchok@gmail.com>
2021-03-25 18:29:43 +0800

    serverless: remove serverless/ directory, and github actions (#30)

--- serverless/prisma/migrations/20210308021137_migration/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
-
-    PRIMARY KEY ("id")
-);
