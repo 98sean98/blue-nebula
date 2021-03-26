50a991b4c449a24ab3b591929e355b6197450dbf
seanchok <seanchok@gmail.com>
2021-03-27 00:54:07 +0800

    mobile: add dangerously override button in RenderBleComponent component so that dev users can ask to show the controller interface in the dev controller screen

--- mobile-app/src/components/shared/bluetooth/RenderBleComponent.tsx
+++ mobile-app/src/components/shared/bluetooth/RenderBleComponent.tsx
@@ -1,21 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { View } from 'react-native';
 import { useSelector } from 'react-redux';
-import { Text } from '@ui-kitten/components';
+import { Button, Text } from '@ui-kitten/components';
 import { useTranslation } from 'react-i18next';
 
 import { tailwind } from '@styles/tailwind';
 
 import { RootState } from '@reduxApp';
 
+import { renderIcon } from '@components/shared/interface';
+
 interface RenderBleComponent {
   overrideShouldShow?: boolean;
   shouldShowHelperText?: boolean;
+  allowDangerousOverride?: boolean;
 }
 
 export const RenderBleComponent: FC<RenderBleComponent> = ({
   overrideShouldShow,
   shouldShowHelperText = true,
+  allowDangerousOverride,
   children,
 }) => {
   const { t } = useTranslation('bluetooth');
@@ -26,9 +30,15 @@ export const RenderBleComponent: FC<RenderBleComponent> = ({
 
   const shouldShow = overrideShouldShow ?? isBleRpiDeviceConnected;
 
+  const [dangerouslyOverrideBle, setDangerouslyOverrideBle] = useState<boolean>(
+    false,
+  );
+
+  const onDangerousButtonPress = () => setDangerouslyOverrideBle(true);
+
   return (
     <>
-      {shouldShow ? (
+      {shouldShow || dangerouslyOverrideBle ? (
         children
       ) : shouldShowHelperText ? (
         <View
@@ -36,6 +46,15 @@ export const RenderBleComponent: FC<RenderBleComponent> = ({
           <Text style={tailwind('text-center')}>
             {t('connection.connect to bluetooth device') as string}
           </Text>
+          {allowDangerousOverride ? (
+            <Button
+              status={'warning'}
+              accessoryLeft={renderIcon('alert-triangle-outline')}
+              onPress={onDangerousButtonPress}
+              style={tailwind('mt-4')}>
+              Dangerously render
+            </Button>
+          ) : null}
         </View>
       ) : null}
     </>
--- mobile-app/src/containers/main/DevControllerScreen.tsx
+++ mobile-app/src/containers/main/DevControllerScreen.tsx
@@ -1,11 +1,14 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useMemo, useState } from 'react';
 import { View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Tab, TabView } from '@ui-kitten/components';
+import { useSelector } from 'react-redux';
 
 import { DevControllerScreenProps } from '@navigation/main';
 
 import { tailwind } from '@styles/tailwind';
+
+import { RootState } from '@reduxApp';
 
 import {
   // RealTimeControlMode,
@@ -18,8 +21,14 @@ export const DevControllerScreen: FC<DevControllerScreenProps> = () => {
 
   const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
+  const { authorizationToken } = useSelector((state: RootState) => state.auth);
+
+  const isLoggedIn = useMemo(() => typeof authorizationToken !== 'undefined', [
+    authorizationToken,
+  ]);
+
   return (
-    <RenderBleComponent>
+    <RenderBleComponent allowDangerousOverride={isLoggedIn}>
       <View style={[{ flex: 1, marginBottom: insets.bottom }]}>
         <TabView
           style={[{ flex: 1 }, tailwind('pt-2')]}
