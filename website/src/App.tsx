8232136ea17d37bdbd1b3f2a8fd11b722a0744cc
seanchok <seanchok@gmail.com>
2021-04-08 22:13:34 +0800

    website: create placeholder landing page

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Hello } from 'components/Hello';
+
+import { robot } from 'assets/images';
 
 function App() {
   return (
-    <div>
-      <Hello run className={'m-4'} />
+    <div
+      className={'w-screen h-screen flex flex-col justify-center items-center'}>
+      <img src={robot} alt={'robot'} className={'w-40 h-40'} />
+      <div className={'mt-3 flex flex-col items-center'}>
+        <h1 className={'text-3xl'}>Blue Nebula</h1>
+        <h3 className={'mt-2 text-xl'}>Coming Soon</h3>
+      </div>
     </div>
   );
 }
---
+++ website/src/assets/images/index.ts
@@ -0,0 +1,3 @@
+import robot from './robot.png';
+
+export { robot };
