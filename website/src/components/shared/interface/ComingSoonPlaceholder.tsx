d17d273b7367b1ebb333de2ff40fa99b28d16262
seanchok <seanchok@gmail.com>
2021-04-10 01:26:20 +0800

    website: simplify coming soon placeholder component

--- website/src/components/shared/interface/ComingSoonPlaceholder.tsx
+++ website/src/components/shared/interface/ComingSoonPlaceholder.tsx
@@ -1,15 +1,16 @@
 import React, { FC, HTMLAttributes } from 'react';
 
-import { robot } from 'assets/images';
+import { combineClassNames } from 'utilities/functions';
 
 export const ComingSoonPlaceholder: FC<HTMLAttributes<HTMLDivElement>> = ({
   ...props
 }) => (
-  <div {...props}>
-    <img src={robot} alt={'robot'} className={'w-40 h-40'} />
-    <div className={'mt-3 flex flex-col items-center'}>
-      <h1 className={'text-3xl'}>Blue Nebula</h1>
-      <h3 className={'mt-2 text-xl'}>Coming Soon</h3>
-    </div>
+  <div
+    {...props}
+    className={combineClassNames(
+      'text-center animate-bounce',
+      props?.className,
+    )}>
+    <h1 className={'mt-2 text-3xl'}>Coming Soon</h1>
   </div>
 );
