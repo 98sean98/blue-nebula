043ed5db30610447e30b09f177c69d2b7f144541
seanchok <seanchok@gmail.com>
2021-04-09 13:24:56 +0800

    website: setup theme colors using the eva design system

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,11 +1,14 @@
+const colors = require('./src/styles/colors');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors,
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
