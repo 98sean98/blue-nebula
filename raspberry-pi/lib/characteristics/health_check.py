e0566ee4dd7955a95433bfd2a32473827d46b389
seanchok <seanchok@gmail.com>
2021-04-20 13:48:41 +0800

    rasp: fix is device connected state in the health check; move buzzer to robot controller service

--- raspberry-pi/lib/characteristics/health_check.py
+++ raspberry-pi/lib/characteristics/health_check.py
@@ -2,8 +2,6 @@ from ..bluetooth.service import Characteristic, Descriptor
 
 from ..config import Config
 
-from ..control_entities.buzzer import Buzzer
-
 from .. import utilities
 
 GATT_CHRC_IFACE = Config.GATT_CHRC_IFACE
@@ -18,21 +16,19 @@ class HealthCheckCharacteristic(Characteristic):
                 ["read", "write"], service)
         self.add_descriptor(HealthCheckDescriptor(self))
 
-        self.buzzer = Buzzer('buzzer', 23, service.multiprocessing_manager)
-
         # start buzzing on initialisation
-        self.buzzer.set_is_running(True)
+        service.set_buzzer(True)
 
     def WriteValue(self, value, options):
         decoded = utilities.decode_base64(value)
         is_device_connected = utilities.parse_string_to_number(decoded) == 1
         self.service.set_is_device_connected(is_device_connected)
         if is_device_connected:
-            self.buzzer.set_is_running(False)
+            self.service.set_buzzer(False)
         print(f"write health_check.is_device_connected = {is_device_connected}")
 
     def ReadValue(self, options):
-        is_device_connected_string = '1' if self.buzzer.get_is_running() else '0'
+        is_device_connected_string = '1' if self.service.get_is_device_connected() else '0'
         health_string = f"{is_device_connected_string}"
         print("read health check:", health_string)
         return utilities.encode_base64(health_string)
--- raspberry-pi/lib/robot_controller_service.py
+++ raspberry-pi/lib/robot_controller_service.py
@@ -5,6 +5,7 @@ from .config import Config
 from .control_entities.stepper_motor import StepperMotor
 from .control_entities.dc_motor import DCMotor
 from .control_entities.bldc_motor import BLDCMotor
+from .control_entities.buzzer import Buzzer
 
 from .characteristics.health_check import HealthCheckCharacteristic
 from .characteristics.ip_address import IPAddressCharacteristic
@@ -17,8 +18,6 @@ SERVICE_UUID = Config.UUID['robot_controller_service']
 
 class RobotControllerService(Service):
     def __init__(self, index, multiprocessing_manager):
-        self.multiprocessing_manager = multiprocessing_manager
-
         self.stepper_motors = {
             'wheel_motor': StepperMotor('wheel_motor', 17, 27, 22, multiprocessing_manager),
             'screw_motor': StepperMotor('screw_motor', 14, 15, 18, multiprocessing_manager)
@@ -28,6 +27,7 @@ class RobotControllerService(Service):
         self.bldc_motors = {
             'bldc_motor': BLDCMotor('bldc_motor', 13, 6, 5, 0, multiprocessing_manager)
         }
+        self.buzzer = Buzzer('buzzer', 23, multiprocessing_manager)
 
         # global states
         self.is_device_connected = False
@@ -66,6 +66,9 @@ class RobotControllerService(Service):
         for motors_of_type in all_motors:
             for motor in list(motors_of_type.values()):
                 motor.set_is_running(is_running)
+
+    def set_buzzer(self, is_running):
+        self.buzzer.set_is_running(is_running)
 
     def get_all_motors(self):
         return {
