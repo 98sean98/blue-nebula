05f2fc864328e85270bec216fd765dd2376ad219
seanchok <seanchok@gmail.com>
2021-05-06 10:41:18 +0800

    rasp: refactor service, characteristics, and utilities to use the control_entities term

--- raspberry-pi/lib/robot_controller_service.py
+++ raspberry-pi/lib/robot_controller_service.py
@@ -12,8 +12,8 @@ from .characteristics.bldc_motor import BLDCMotorsCharacteristic
 
 SERVICE_UUID = BluetoothConfig.UUID['robot_controller_service']
 
-def parse_entities(entity):
-    return entity if entity is not None else {}
+def parse_entities(control_entities, control_entity_type, is_none_allowed = False):
+    return control_entities[control_entity_type] if control_entity_type in control_entities and control_entities[control_entity_type] is not None else None if is_none_allowed else {}
 
 class RobotControllerService(Service):
     def __init__(self, index, multiprocessing_manager):
@@ -21,11 +21,15 @@ class RobotControllerService(Service):
         microApp = MicroApp()
         control_entities = microApp.get_control_entities(multiprocessing_manager)
 
-        self.stepper_motors = parse_entities(control_entities['stepper_motors'])
-        self.dc_motors = parse_entities(control_entities['dc_motors'])
-        self.bldc_motors = parse_entities(control_entities['bldc_motors'])
+        self.control_entities = {
+            'stepper_motors': parse_entities(control_entities, 'stepper_motors'),
+            'dc_motors': parse_entities(control_entities, 'dc_motors'),
+            'bldc_motors': parse_entities(control_entities, 'bldc_motors'),
+            'relays':
+            parse_entities(control_entities, 'relays')
+        }
 
-        self.buzzer = control_entities['buzzer'] # can be None
+        self.buzzer = parse_entities(control_entities, 'buzzer', True)
 
         # global states
         self.is_device_connected = False
@@ -47,10 +51,10 @@ class RobotControllerService(Service):
 
     def get_is_running(self):
         is_running = False
-        all_motors = list(self.get_all_motors().values())
-        for motors_of_type in all_motors:
-            for motor in list(motors_of_type.values()):
-                if motor.get_is_running():
+        all_control_entities = list(self.get_all_control_entities().values())
+        for control_entities_of_type in all_control_entities:
+            for control_entity in list(control_entities_of_type.values()):
+                if control_entity.get_is_running():
                     is_running = True
                     break
         self.is_running = is_running
@@ -59,41 +63,34 @@ class RobotControllerService(Service):
     def set_is_running(self, is_running):
         print(f"service is_running is now {is_running}")
         self.is_running = is_running
-        # set each motor's is_running states
-        all_motors = list(self.get_all_motors().values())
-        for motors_of_type in all_motors:
-            for motor in list(motors_of_type.values()):
-                motor.set_is_running(is_running)
+        # set each control entity's is_running states
+        all_control_entities = list(self.get_all_control_entities().values())
+        for control_entities_of_type in all_control_entities:
+            for control_entity in list(control_entities_of_type.values()):
+                control_entity.set_is_running(is_running)
 
     def set_buzzer(self, is_running):
         if self.buzzer is not None:
             self.buzzer.set_is_running(is_running)
 
-    def get_all_motors(self):
-        return {
-            'stepper_motors': self.stepper_motors,
-            'dc_motors': self.dc_motors,
-            'bldc_motors': self.bldc_motors
-        }
+    def get_all_control_entities(self):
+        return self.control_entities
 
-    def get_motor(self, motor_type, motor_name):
-        motors_of_type = None
-        if motor_type == 'stepper_motor': motors_of_type = self.stepper_motors
-        if motor_type == 'dc_motor': motors_of_type = self.dc_motors
-        if motor_type == 'bldc_motor': motors_of_type = self.bldc_motors
+    def get_control_entity(self, control_entity_type, entity_name):
+        control_entities_of_type = self.get_all_control_entities()[control_entity_type]
 
-        if motors_of_type is None:
-            print(f"{motor_type} is not a valid motor type")
+        if control_entities_of_type is None:
+            print(f"{control_entity_type} is not a valid control entity type")
             return None
 
-        if motor_name not in list(motors_of_type.keys()):
-            print(f"{motor_name} cannot be found in the service")
+        if entity_name not in list(control_entities_of_type.keys()):
+            print(f"{entity_name} cannot be found in the service")
             return None
 
-        return motors_of_type[motor_name]
+        return control_entities_of_type[entity_name]
 
-    def set_motor(self, motor_type, motor_name, parameters):
-        motor = self.get_motor(motor_type, motor_name)
-        if motor is None:
+    def set_control_entity(self, control_entity_type, entity_name, parameters):
+        control_entity = self.get_control_entity(control_entity_type, entity_name)
+        if control_entity is None:
             return
-        motor.set_parameters(parameters)
+        control_entity.set_parameters(parameters)
