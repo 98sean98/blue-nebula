afffc4f10179d5fd20a93530e43f198a0ea3ad90
Sean Chok <seanchok@gmail.com>
2021-06-02 03:53:13 +0100

    mobile: bump release version to v1.0.0 (#65)

--- mobile-app/src/config/environment.ts
+++ mobile-app/src/config/environment.ts
@@ -6,9 +6,8 @@ export const serverUrl = {
   graphql: `${main}/graphql`,
 };
 
-// for now, manually update the release tag in conjunction with the tag on the latest release on github repo
-// todo: abstract this to use an environment variable that is updated on ci/cd
-export const releaseTag = 'v0.1.1';
+// manually update the release tag in conjunction with the tag on the latest release on github repo
+export const releaseTag = 'v1.0.0';
 
 export const appDownloadLink =
   'https://drive.google.com/drive/folders/1ZJXtwy7sIi97cVVAn4BY7bHpVlrG1_z7?usp=sharing';
