730cd4e0082b19f6655bb163bba15447bb094a67
seanchok <seanchok@gmail.com>
2021-06-07 17:52:23 +0800

    mobile: replace app download link

--- mobile-app/src/config/environment.ts
+++ mobile-app/src/config/environment.ts
@@ -9,5 +9,4 @@ export const serverUrl = {
 // manually update the release tag in conjunction with the tag on the latest release on github repo
 export const releaseTag = 'v1.0.0';
 
-export const appDownloadLink =
-  'https://drive.google.com/drive/folders/1ZJXtwy7sIi97cVVAn4BY7bHpVlrG1_z7?usp=sharing';
+export const appDownloadLink = 'https://eisc.towngas.com/APK/src.apk';
