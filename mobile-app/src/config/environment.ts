68d5e3c89a47e786c49cf88a74f42f767edfacb3
Sean Chok <seanchok@gmail.com>
2021-06-10 10:43:21 +0100

    mobile: update app download link (#68)
    * mobile: replace app download link
    
    * mobile: bump release version to v1.0.1
    
    * mobile: abstract github tags url to environment config

--- mobile-app/src/config/environment.ts
+++ mobile-app/src/config/environment.ts
@@ -7,7 +7,8 @@ export const serverUrl = {
 };
 
 // manually update the release tag in conjunction with the tag on the latest release on github repo
-export const releaseTag = 'v1.0.0';
+export const releaseTag = 'v1.0.1';
+export const githubTagsUrl =
+  'https://api.github.com/repos/98sean98/blue-nebula/tags';
 
-export const appDownloadLink =
-  'https://drive.google.com/drive/folders/1ZJXtwy7sIi97cVVAn4BY7bHpVlrG1_z7?usp=sharing';
+export const appDownloadLink = 'https://eisc.towngas.com/APK/src.apk';
